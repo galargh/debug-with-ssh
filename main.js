cf9962bbdb3f57c0c53603c00baa4ba8db6f3571
galargh <piotr.galar@gmail.com>
2023-12-22 22:56:20 +0100

    feat: install tmux

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const tc = require('@actions/tool-cache')
+const exec = require('@actions/exec')
 
 const os = require('os')
 const path = require('path')
@@ -97,13 +98,54 @@ async function installCloudflared(platform, arch, version) {
   core.addPath(dirPath)
 }
 
+async function installTmux(platform, arch, version) {
+  let tmuxPath = tc.find('tmux', version, arch)
+  if (tmuxPath !== '') {
+    core.info(`Found cached tmux(${version}) for ${platform}(${arch}) at ${tmuxPath}`)
+  } else {
+    let binPath
+    switch(platform) {
+      case 'linux':
+        // Assuming Ubuntu
+        await exec.exec('sudo apt update')
+        await exec.exec('sudo apt remove -y tmux')
+        await exec.exec(`sudo apt install -y tmux=${version}`)
+        binPath = await exec.getExecOutput('which tmux')
+        break
+      case 'darwin':
+        await exec.exec('brew update')
+        await exec.exec(`brew install --force tmux@${version}`)
+        binPath = await exec.getExecOutput('which tmux')
+        break
+      case 'windows':
+        await exec.exec(`pacman -Sy --noconfirm tmux=${version}`, undefined, {shell: 'C:\\msys64\\usr\\bin\\bash.exe'})
+        binPath = await exec.getExecOutput('which tmux', undefined, {shell: 'C:\\msys64\\usr\\bin\\bash.exe'})
+        break
+      default:
+        throw new Error(`Unsupported platform: ${platform}`)
+    }
+
+    core.info(`Caching ${binPath}`)
+    cachePath = await tc.cacheFile(binPath, 'tmux', 'tmux', version, arch)
+
+    tmuxPath = path.join(cachePath, 'tmux')
+  }
+
+  dirPath = path.dirname(tmuxPath)
+
+  core.info(`Adding ${dirPath} which contains ${tmuxPath} to PATH`)
+  core.addPath(dirPath)
+}
+
 async function run() {
   try {
-    const version = core.getInput('version')
+    const cloudflaredVersion = core.getInput('cloudflared')
+    const tmuxVersion = core.getInput('tmux')
 
     const [platform, arch] = getPlatformAndArch()
 
-    await installCloudflared(platform, arch, version)
+    await installCloudflared(platform, arch, cloudflaredVersion)
+    await installTmux(platform, arch, tmuxVersion)
   } catch (error) {
     core.setFailed(error.message)
   }
